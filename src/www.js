9b10d1fafbafa4ca8cb1c12b3c9a68ee9309c47e
Soheil Rashidi <contact@soheilrashidi.com>
2016-06-14 10:09:18 +0430

    Renamed to Nautilus.

--- src/www.js
+++ src/www.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-var debug = require('debug')('odin-web');
+var debug = require('debug')('nautilus-web');
 var config = require('./config');
 var app = require('./app');
 
 var server = app.listen(config.get('port'), function() {
-  debug('Odin web listening on port ' + server.address().port);
+  debug('Nautilus web listening on port ' + server.address().port);
 });
