34a4ce64bcb8739cff19a686e4af1aeaa96aca42
Soheil Rashidi <contact@soheilrashidi.com>
2016-08-05 11:00:03 +0430

    Moved Issues to its own component.

--- src/assets/app/scripts/components/app.tsx
+++ src/assets/app/scripts/components/app.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import { Nautilus } from '../nautilus';
-import { FilterSet } from '../filter';
-import { FilterBox } from './filter-box';
-import { FilterableIssueList } from './filterable-issue-list';
+import { Issues } from './issues';
 
 interface AppState {
   isInitialized?;
   error?;
-  filters?;
 }
 
 export class App extends React.Component<{}, AppState> {
@@ -16,18 +13,10 @@ export class App extends React.Component<{}, AppState> {
 
     this.state = {
       isInitialized: false,
-      filters: new FilterSet(['milestones', 'states', 'assignedUsers', 'projects'])
     };
   }
 
   componentDidMount() {
-    document.addEventListener("keydown", (event) => {
-      if (event.which === 78 && event.getModifierState('Control')) { // Ctrl+N
-        this.addIssue();
-        event.preventDefault();
-      }
-    }, false);
-
     Nautilus.Instance.on('error', (error) => {
       this.setState({
         error: error
@@ -41,14 +30,6 @@ export class App extends React.Component<{}, AppState> {
     });
 
     Nautilus.Instance.init();
-  }
-
-  onFiltersChanged() {
-    this.forceUpdate();
-  }
-
-  addIssue() {
-    Nautilus.Instance.addIssue({});
   }
 
   render() {
@@ -65,27 +46,7 @@ export class App extends React.Component<{}, AppState> {
       );
 
     return (
-      <div>
-        <div style={{marginBottom: '20px'}} className='row'>
-          <div className='two columns'>
-            &nbsp;
-          </div>
-          <div className='ten columns'>
-            <button title='Ctrl+N' className="button-primary" onClick={this.addIssue.bind(this)}>Add Issue</button>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='two columns sidebar'>
-            <FilterBox name='Milestone' items={Nautilus.Instance.getMilestones()} displayAttribute='title' filter={this.state.filters.milestones} filters={this.state.filters} onChanged={this.onFiltersChanged.bind(this)} />
-            <FilterBox name='State' items={Nautilus.Instance.getStates()} displayAttribute='title' filter={this.state.filters.states} filters={this.state.filters} onChanged={this.onFiltersChanged.bind(this)} />
-            <FilterBox name='Assignee' items={Nautilus.Instance.getUsers()} displayAttribute='name' filter={this.state.filters.assignedUsers} filters={this.state.filters} onChanged={this.onFiltersChanged.bind(this)} />
-            <FilterBox name='Project' items={Nautilus.Instance.getProjects()} displayAttribute='name' filter={this.state.filters.projects} filters={this.state.filters} onChanged={this.onFiltersChanged.bind(this)} />
-          </div>
-          <div className='ten columns'>
-            <FilterableIssueList filters={this.state.filters} />
-          </div>
-        </div>
-      </div>
+      <Issues />
     );
   }
 };
---
+++ src/assets/app/scripts/components/issues.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { Nautilus } from '../nautilus';
+import { FilterSet } from '../filter';
+import { FilterBox } from './filter-box';
+import { FilterableIssueList } from './filterable-issue-list';
+
+interface IssuesState {
+  filters?;
+}
+
+export class Issues extends React.Component<{}, IssuesState> {
+  constructor() {
+    super();
+
+    this.state = {
+      filters: new FilterSet(['milestones', 'states', 'assignedUsers', 'projects'])
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", (event) => {
+      if (event.which === 78 && event.getModifierState('Control')) { // Ctrl+N
+        this.addIssue();
+        event.preventDefault();
+      }
+    }, false);
+  }
+
+  onFiltersChanged() {
+    this.forceUpdate();
+  }
+
+  addIssue() {
+    Nautilus.Instance.addIssue({});
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{marginBottom: '20px'}} className='row'>
+          <div className='two columns'>
+            &nbsp;
+          </div>
+          <div className='ten columns'>
+            <button title='Ctrl+N' className="button-primary" onClick={this.addIssue.bind(this)}>Add Issue</button>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='two columns sidebar'>
+            <FilterBox name='Milestone' items={Nautilus.Instance.getMilestones()} displayAttribute='title' filter={this.state.filters.milestones} filters={this.state.filters} onChanged={this.onFiltersChanged.bind(this)} />
+            <FilterBox name='State' items={Nautilus.Instance.getStates()} displayAttribute='title' filter={this.state.filters.states} filters={this.state.filters} onChanged={this.onFiltersChanged.bind(this)} />
+            <FilterBox name='Assignee' items={Nautilus.Instance.getUsers()} displayAttribute='name' filter={this.state.filters.assignedUsers} filters={this.state.filters} onChanged={this.onFiltersChanged.bind(this)} />
+            <FilterBox name='Project' items={Nautilus.Instance.getProjects()} displayAttribute='name' filter={this.state.filters.projects} filters={this.state.filters} onChanged={this.onFiltersChanged.bind(this)} />
+          </div>
+          <div className='ten columns'>
+            <FilterableIssueList filters={this.state.filters} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
