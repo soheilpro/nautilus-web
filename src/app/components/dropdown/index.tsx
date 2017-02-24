4e0cc24a29bdd3ce8520eceafd3c2593e9fbddbf
Soheil Rashidi <contact@soheilrashidi.com>
2017-02-24 12:26:39 +0330

    ensure-dropdown-visible

--- src/app/components/dropdown/index.tsx
+++ src/app/components/dropdown/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import * as classNames from 'classnames';
 import Icon from '../icon';
 import Window, { WindowContainer } from '../window';
@@ -18,7 +19,9 @@ interface IDropdownState {
 }
 
 export default class Dropdown extends React.Component<IDropdownProps, IDropdownState> {
-  private rootElement?: HTMLElement;
+  private rootElement: HTMLElement;
+  private buttonElement: HTMLElement;
+  private windowContainerComponent: WindowContainer;
 
   constructor() {
     super();
@@ -27,6 +30,20 @@ export default class Dropdown extends React.Component<IDropdownProps, IDropdownS
     this.handleWindowContainerCloseRequest = this.handleWindowContainerCloseRequest.bind(this);
 
     this.state = {};
+  }
+
+  componentDidUpdate() {
+    if (!this.windowContainerComponent)
+      return;
+
+    let windowContainerElement = ReactDOM.findDOMNode(this.windowContainerComponent) as HTMLElement;
+    let windowContainerElementLeft = windowContainerElement.offsetLeft;
+    let windowContainerElementWidth = windowContainerElement.offsetWidth;
+
+    if (windowContainerElementLeft + windowContainerElementWidth <= window.innerWidth)
+      return;
+
+    windowContainerElement.style.left = (this.buttonElement.offsetLeft + this.buttonElement.offsetWidth - windowContainerElementWidth) + 'px';
   }
 
   open() {
@@ -61,13 +78,13 @@ export default class Dropdown extends React.Component<IDropdownProps, IDropdownS
   render() {
     return (
       <div className={classNames('dropdown-component', this.props.className, {'open': this.state.isOpen})} tabIndex={0} ref={e => this.rootElement = e}>
-        <div className="button" onClick={this.handleButtonClick}>
+        <div className="button" onClick={this.handleButtonClick} ref={e => this.buttonElement = e}>
           {this.props.title}
           <Icon className="caret" name={this.state.isOpen ? 'caret-up' : 'caret-down'} />
         </div>
         {
           this.state.isOpen &&
-            <WindowContainer position="absolute" blurCheckElement={this.rootElement} onCloseRequest={this.handleWindowContainerCloseRequest}>
+            <WindowContainer position="absolute" blurCheckElement={this.rootElement} onCloseRequest={this.handleWindowContainerCloseRequest} ref={e => this.windowContainerComponent = e}>
               <Window className="window">
                 {this.props.children}
               </Window>
