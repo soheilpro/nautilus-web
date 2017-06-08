3f169d7b3f548e38d3fc0f8aac6cb32010975f6d
Soheil Rashidi <contact@soheilrashidi.com>
2017-06-09 00:24:55 +0430

    webpack

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const webpack = require('webpack');
 const failPlugin = require('webpack-fail-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -9,24 +10,21 @@ module.exports = {
     vendor: ['react', 'react-dom', 'react-router', 'axios', 'underscore', 'wolfy87-eventemitter', 'blueimp-md5', 'classnames', 'uuid', 'jquery', 'jquery-sticky'],
   },
   output: {
-    path: './out/assets',
+    path: path.resolve(__dirname, './out/assets'),
     filename: '[name].js',
   },
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
   },
   module: {
     loaders: [
       { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
-      { test: /\.less$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader') },
+      { test: /\.less$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!less-loader' }) },
       { test: /\.ttf(\?\S*)?$/, loader: 'file-loader?name=[name].[ext]' },
       { test: /\.svg(\?\S*)?$/, loader: 'file-loader?name=[name].[ext]' },
       { test: /\.woff2?(\?\S*)?$/, loader: 'file-loader?name=[name].[ext]' },
       { test: /\.eot(\?\S*)?$/, loader: 'file-loader?name=[name].[ext]' },
       { test: /\.html$/, loader: 'html-loader' },
-    ],
-    preLoaders: [
-      { test: /\.js$/, loader: "source-map-loader" },
     ],
   },
   plugins: [
@@ -35,7 +33,7 @@ module.exports = {
       $: "jquery",
     }),
     new ExtractTextPlugin('[name].css'),
-    new webpack.optimize.CommonsChunkPlugin('vendor', '[name].js'),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: '[name].js' }),
     new HtmlWebpackPlugin({
       filename: '../index.ejs',
       template: './app/index.html',
