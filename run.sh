9ecfab4c7691eb70592d600f40219f366468cac1
Soheil Rashidi <contact@soheilrashidi.com>
2017-08-29 23:45:08 +0430

    npm-run-silent

--- run.sh
+++ run.sh
@@ -3,6 +3,14 @@
 COMMAND=$1
 
 case "$COMMAND" in
+  build)
+    (cd ./src && npm run --silent build)
+    ;;
+
+  clean)
+    (cd ./src && npm run --silent clean)
+    ;;
+
   config)
     "${EDITOR:-vi}" ${NAUTILUS_WEB_CONFIG:-./config/app.json}
     ;;
@@ -11,33 +19,25 @@ case "$COMMAND" in
     (cd ./src && npm install)
     ;;
 
-  build)
-    (cd ./src && npm run build)
-    ;;
-
   release)
-    (cd ./src && npm run release)
-    ;;
-
-  clean)
-    (cd ./src && npm run clean)
+    (cd ./src && npm run --silent release)
     ;;
 
   start)
-    (cd ./src && npm run start)
+    (cd ./src && npm run --silent start)
     ;;
 
   watch)
-    (cd ./src && npm run watch)
+    (cd ./src && npm run --silent watch)
     ;;
 
   *)
     echo "Usage:"
+    echo "  $(basename "$0") build"
+    echo "  $(basename "$0") clean"
     echo "  $(basename "$0") config"
     echo "  $(basename "$0") install"
-    echo "  $(basename "$0") build"
     echo "  $(basename "$0") release"
-    echo "  $(basename "$0") clean"
     echo "  $(basename "$0") start"
     echo "  $(basename "$0") watch"
     exit 1
